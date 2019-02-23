d1f3f301c487363fd06d3c0990fb1906304b019a
Elliott Coleman <1705665@brunel.ac.uk>
2019-02-23 16:30:30 +0000

    left navigatoin

--- prospect/src/index.js
+++ prospect/src/index.js
@@ -7,8 +7,7 @@ import ListPeople from './ListPeople'
 
 import './App.css';
 
-import { Row, Col, Icon } from 'antd';
-
+import { Row, Col, Icon, Menu } from 'antd';
 
 import * as serviceWorker from './serviceWorker';
 
@@ -18,9 +17,20 @@ import {
   Link
 } from 'react-router-dom'
 
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
 
 
 class AppRouter extends React.Component {
+	state = {
+		expanded: false
+	}
+
+	toggleLeft = () => {
+		this.setState({expanded: !this.state.expanded})
+		console.log("expanded")
+	}
 
   render() {
     return (
@@ -28,10 +38,24 @@ class AppRouter extends React.Component {
       	<div>
       		{/* <Route path={`/john`} component={John}/> */}
 	        <Row style={{height: 40, marginLeft: 10, marginRight: 10}}>
-	          <Col span={2}><Icon type="bars" style={{position: 'absolute', fontSize: 20, marginTop: 10, color: '#91268F'}} /></Col>
+	          <Col span={2}><Icon onClick={() => this.toggleLeft()} type="bars" style={{position: 'absolute', fontSize: 20, marginTop: 10, color: '#91268F'}} /></Col>
 	          <Col span={6} />
 	          <Col span={8} style={{paddingTop: 0, textAlign: 'center'}}><span style={{color: '#91268F', fontWeight: 600, fontSize: 25, }}>Prospect</span></Col>
 	        </Row>
+
+	        {this.state.expanded && (
+	        	<Menu
+			        onClick={this.handleClick}
+			        style={{ width: 256, position: 'absolute', zIndex: 10, height: '96%' }}
+			        defaultSelectedKeys={['1']}
+			        defaultOpenKeys={['sub1']}
+			        mode="inline"
+			      >
+			        <Menu.Item key="mentees" ><span><Icon type="message" theme="filled" /><span>Mentees</span></span></Menu.Item>
+			        <Menu.Item key="connect" ><span><Icon type="user-add" /><span>Connect</span></span></Menu.Item>
+			        <Menu.Item key="myprofile" ><span><Icon type="idcard" theme="filled" /><span>My Profile</span></span></Menu.Item>
+			      </Menu>
+	        )}
 	        
 	        <div className="App">
 	       	 <Route path="/john" component={John}/>
